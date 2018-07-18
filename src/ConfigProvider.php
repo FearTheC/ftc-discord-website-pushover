2babb9440a32e0ba5286568dc58eb6657e9bcb0e
Quentin Bonaventure <quentin@bonaventure.xyz>
2018-07-18 14:33:01 +0200

    Added missing PSR container interfaces

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace FTC\Notifier\Pushover;
 
+use Psr\Container\ContainerInterface;
+use LeonardoTeixeira\Pushover\Client;
+
 class ConfigProvider
 {
 
@@ -26,7 +29,7 @@ class ConfigProvider
             'factories'  => [
                 'PushoverClient' => function(ContainerInterface $container) {
                     $config = $container->get('config')->offsetGet('pushover');
-                    return new LeonardoTeixeira\Pushover\Client($config['user_id'], $config['app_token']);
+                    return new Client($config['user_id'], $config['app_token']);
                 },
             ],
         ];
